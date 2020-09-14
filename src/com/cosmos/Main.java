8bbd6c848cb2a4f67353b901c67ac8dec86afb0e
Cosmos <XinyueFeng_UESTC@outlook.com>
2020-09-14 21:36:53 +0800

    Edited some details
    Signed-off-by: Cosmos <XinyueFeng_UESTC@outlook.com>

--- src/com/cosmos/Main.java
+++ src/com/cosmos/Main.java
@@ -1,8 +1,12 @@
 package com.cosmos;
 
+/**
+ * @author Cosmos_F
+ */
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
+
